3bdb900c011bc28d32fe2eb9c6bd976421e8d9a4
Manish Jangid <158812987+manishjangid2005@users.noreply.github.com>
2024-10-13 11:37:58 +0530

    Add files via upload

---
+++ Chapter 1 Problem/problem2-ch1.py
@@ -0,0 +1,7 @@
+# Print a table of 5 using repal method
+# repal mean terminal
+
+# run python in terminal
+# do 5*1
+# 5*2
+# type
---
+++ Chapter 1 Problem/problem3-ch1.py
@@ -0,0 +1,7 @@
+# write a program with the help any module like frameworks
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("You are my reminder")
+engine.runAndWait()
+
